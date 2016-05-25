aafa1dc832f57317d4b50d403d3acccc66d0f9a8
Ashraf <ashraf@Ashrafs-MBP.wks.jnj.com>
2016-05-25 15:00:48 -0400

    test

---
+++ js/task.js
@@ -0,0 +1,43 @@
+// Code goes here
+
+var papa_parse = require('.papa_parse.min.js');
+console.log(papa_parse);
+// instant crawler
+var iCrawler = {
+  
+};
+
+// traditional crawler
+var tCrawler = {
+  
+};
+
+// convert csv to json
+var csvToJson = {
+  
+};
+
+// json to chunks
+var jsonToChunks = {
+  
+};
+
+// parse chunks
+var parseChunks = {
+  
+};
+
+// filter information
+var filterInfo = {
+  
+};
+
+// ajax fetch
+var xhrf = {
+  
+};
+
+// handle uploads
+var hUploads = {
+  
+};
